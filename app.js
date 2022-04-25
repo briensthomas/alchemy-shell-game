b8ac539e7426f3a64e15b978a09272e94883150b
Brien Thomas <thomasbrien94@gmail.com>
2022-04-25 16:11:44 -0700

    Created java syntax for first shell

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-// import functions and grab DOM elements
+const button1 = document.getElementById('button1');
+console.log(button1)
 
-// let state
+const correctSpan = document.getElementById('correctSpan');
+const incorrectSpan = document.getElementById('incorrectSpan');
+const totalSpan = document.getElementById('totalSpan');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+let correct = 0;
+let incorrect = 0;
+
+button1.addEventListener('click', () => {
+    console.log('clicked button');
+
+    const randomShell = Math.ceil(Math.random() * 3);
+    console.log('The random shell is ', randomShell);
+// if randomShell = 1, increase the win counter
+    if (randomShell === 1) {
+        correct++;
+    } else if (randomShell === 2) {
+        incorrect++;
+    } else {
+        incorrect++;
+    }
+    correctSpan.textContent = correct;
+    incorrectSpan.textContent = incorrect;
+    totalSpan.textContent = correct + incorrect;
+});
